95f0a3f0b3a39bb4abdbc73fb0acf49ea6bdd079
Tayyab <tayabodla786@gmail.com>
2025-10-30 11:03:50 +0500

    Update calculator UI and config

--- src/App.jsx
+++ src/App.jsx
@@ -24,31 +24,32 @@ function Calculator () {
   }
 
   return (
+<>
+    <h1 className=' font-bold font-serif text-6xl w-82 flex justify-center h-18  bg-white/20 backdrop-blur-md  placeholder-white/70 border border-white/30 focus:outline-none focus:ring-2 focus:ring-amber-300 ml-116 mt-9 rounded-2xl text-black'>Calculator</h1>
 
-
-    <div className='h-130 w-120 text-4xl flex flex-col justify-center items-center bg-amber-300  '>
+    <div className='h-92 w-74 rounded-lg m-0 p-0 text-4xl flex flex-col  justify-center shadow-lg mt-12 ml-120  bg-white/20 backdrop-blur-md text-white placeholder-white/70 border border-white/30 focus:outline-none focus:ring-2 focus:ring-amber-300 '>
       
-        <input className='w-90 h-20 mb-1.5 pl-2 text-4x2 rounded-3xl bg-amber-950 ' type="text"  value={display.replace(/\*/g, "×").replace(/\//g, "÷")} readOnly />
-      <div className='w-90 h-90 flex flex-wrap justify-center rounded-3xl bg-amber-50 p-0'>
-        <button className='text-4xl m-0.5 w-20 h-20 rounded-full bg-amber-200' onClick={() => addValueToDisplay('7')}>7</button>
-        <button className='text-4xl m-0.5 w-20 h-20 rounded-full bg-amber-200'  onClick={() => addValueToDisplay('8')}>8</button>
-        <button className='text-4xl m-0.5 w-20 h-20 rounded-full bg-amber-200' onClick={() => addValueToDisplay('9')}>9</button>
-        <button className='text-4xl m-0.5 w-20 h-20 rounded-full bg-amber-200' onClick={() => addValueToDisplay('*')}>×</button>
-        <button className='text-4xl m-0.5 w-20 h-20 rounded-full bg-amber-200' onClick={() => addValueToDisplay('4')}>4</button>
-        <button className='text-4xl m-0.5 w-20 h-20 rounded-full bg-amber-200' onClick={() => addValueToDisplay('5')}>5</button>
-        <button className='text-4xl m-0.5 w-20 h-20 rounded-full bg-amber-200' onClick={() => addValueToDisplay('6')}>6</button>
-        <button className='text-4xl m-0.5 w-20 h-20 rounded-full bg-amber-200' onClick={() => addValueToDisplay('/')}>÷</button>
-        <button className='text-4xl m-0.5 w-20 h-20 rounded-full bg-amber-200' onClick={() => addValueToDisplay('1')}>1</button>
-        <button className='text-4xl m-0.5 w-20 h-20 rounded-full bg-amber-200' onClick={() => addValueToDisplay('2')}>2</button>
-        <button className='text-4xl m-0.5 w-20 h-20 rounded-full bg-amber-200' onClick={() => addValueToDisplay('3')}>3</button>
-        <button className='text-4xl m-0.5 w-20 h-20 rounded-full bg-amber-200' onClick={() => addValueToDisplay('-')}>-</button>
-        <button className='text-4xl m-0.5 w-20 h-20 rounded-full bg-amber-200' onClick={clearDisplay}>C</button>
-        <button className='text-4xl m-0.5 w-20 h-20 rounded-full bg-amber-200' onClick={() => addValueToDisplay('0')}>0</button>
-        <button className='text-4xl m-0.5 w-20 h-20 rounded-full bg-amber-200' onClick={() => addValueToDisplay('+')}>+</button>
-        <button className='text-4xl m-0.5 w-20 h-20 rounded-full bg-amber-200' onClick={calculate}>=</button>
+        <input className='w-70 p-3 rounded-2xl ml-2 mb-3 bg-white/20 backdrop-blur-md text-black placeholder-white/70 border border-white/30 focus:outline-none focus:ring-2 focus:ring-amber-300 ' type="text"  value={display.replace(/\*/g, "×").replace(/\//g, "÷")} readOnly />
+      <div className='flex flex-wrap justify-center gap-1'>
+        <button className='w-16 h-16 rounded-full font-serif shadow-2xl shadow-blue-50 transition duration-200 text-black bg-white/20 backdrop-blur-md placeholder-white/70  focus:ring-2 focus:ring-amber-300 text-2xl font-bold hover:bg-amber-300' onClick={() => addValueToDisplay('7')}>7</button>
+        <button className='w-16 h-16 rounded-full font-serif shadow-2xl shadow-blue-50 transition duration-200 text-black bg-white/20 backdrop-blur-md placeholder-white/70  focus:ring-2 focus:ring-amber-300 text-2xl font-bold hover:bg-amber-300'  onClick={() => addValueToDisplay('8')}>8</button>
+        <button className='w-16 h-16 rounded-full font-serif shadow-2xl shadow-blue-50 transition duration-200 text-black bg-white/20 backdrop-blur-md placeholder-white/70  focus:ring-2 focus:ring-amber-300 text-2xl font-bold hover:bg-amber-300' onClick={() => addValueToDisplay('9')}>9</button>
+        <button className='w-16 h-16 rounded-full font-serif shadow-2xl shadow-blue-50 transition duration-200 text-black bg-white/20 backdrop-blur-md placeholder-white/70  focus:ring-2 focus:ring-amber-300 text-2xl font-bold hover:bg-amber-300' onClick={() => addValueToDisplay('*')}>×</button>
+        <button className='w-16 h-16 rounded-full font-serif shadow-2xl shadow-blue-50 transition duration-200 text-black bg-white/20 backdrop-blur-md placeholder-white/70  focus:ring-2 focus:ring-amber-300 text-2xl font-bold hover:bg-amber-300' onClick={() => addValueToDisplay('4')}>4</button>
+        <button className='w-16 h-16 rounded-full font-serif shadow-2xl shadow-blue-50 transition duration-200 text-black bg-white/20 backdrop-blur-md placeholder-white/70  focus:ring-2 focus:ring-amber-300 text-2xl font-bold hover:bg-amber-300' onClick={() => addValueToDisplay('5')}>5</button>
+        <button className='w-16 h-16 rounded-full font-serif shadow-2xl shadow-blue-50 transition duration-200 text-black bg-white/20 backdrop-blur-md placeholder-white/70  focus:ring-2 focus:ring-amber-300 text-2xl font-bold hover:bg-amber-300' onClick={() => addValueToDisplay('6')}>6</button>
+        <button className='w-16 h-16 rounded-full font-serif shadow-2xl shadow-blue-50 transition duration-200 text-black bg-white/20 backdrop-blur-md placeholder-white/70  focus:ring-2 focus:ring-amber-300 text-2xl font-bold hover:bg-amber-300' onClick={() => addValueToDisplay('/')}>÷</button>
+        <button className='w-16 h-16 rounded-full font-serif shadow-2xl shadow-blue-50 transition duration-200 text-black bg-white/20 backdrop-blur-md placeholder-white/70  focus:ring-2 focus:ring-amber-300 text-2xl font-bold hover:bg-amber-300' onClick={() => addValueToDisplay('1')}>1</button>
+        <button className='w-16 h-16 rounded-full font-serif shadow-2xl shadow-blue-50 transition duration-200 text-black bg-white/20 backdrop-blur-md placeholder-white/70  focus:ring-2 focus:ring-amber-300 text-2xl font-bold hover:bg-amber-300' onClick={() => addValueToDisplay('2')}>2</button>
+        <button className='w-16 h-16 rounded-full font-serif shadow-2xl shadow-blue-50 transition duration-200 text-black bg-white/20 backdrop-blur-md placeholder-white/70  focus:ring-2 focus:ring-amber-300 text-2xl font-bold hover:bg-amber-300' onClick={() => addValueToDisplay('3')}>3</button>
+        <button className='w-16 h-16 rounded-full font-serif shadow-2xl shadow-blue-50 transition duration-200 text-black bg-white/20 backdrop-blur-md placeholder-white/70  focus:ring-2 focus:ring-amber-300 text-2xl font-bold hover:bg-amber-300' onClick={() => addValueToDisplay('-')}>-</button>
+        <button className='w-16 h-16 rounded-full font-serif shadow-2xl shadow-blue-50 transition duration-200 text-black bg-amber-300 backdrop-blur-md placeholder-white/70  focus:ring-2 focus:ring-amber-300 text-md font-bold hover:bg-white/20' onClick={clearDisplay}>C</button>
+        <button className='w-16 h-16 rounded-full font-serif shadow-2xl shadow-blue-50 transition duration-200 text-black bg-white/20 backdrop-blur-md placeholder-white/70  focus:ring-2 focus:ring-amber-300 text-2xl font-bold hover:bg-amber-300' onClick={() => addValueToDisplay('0')}>0</button>
+        <button className='w-16 h-16 rounded-full font-serif shadow-2xl shadow-blue-50 transition duration-200 text-black bg-white/20 backdrop-blur-md placeholder-white/70  focus:ring-2 focus:ring-amber-300 text-2xl font-bold hover:bg-amber-300' onClick={() => addValueToDisplay('+')}>+</button>
+        <button className='w-16 h-16 rounded-full font-serif shadow-2xl shadow-blue-50 transition duration-200 text-black bg-white/20 backdrop-blur-md placeholder-white/70  focus:ring-2 focus:ring-amber-300 text-2xl font-bold hover:bg-amber-300' onClick={calculate}>=</button>
       </div>
     </div>
-    
+    </>
   )
 }
 
--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,10 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
+
+
 // https://vite.dev/config/
 export default defineConfig({
+  base: "/React-Calculator/",
   plugins: [react(), tailwindcss()],
 })
